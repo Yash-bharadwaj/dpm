05392f368e41ea0a688bafb7e219ac5c9ebec843
Yashwanth <ymantha@blusapphire.com>
2024-07-24 11:17:32 +0530

    heart beat api integrated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
-import { useQuery } from '@apollo/client';
-import { GET_DEVICES_LIST } from '../query/query';
+import { useQuery, useMutation } from '@apollo/client';
+import { useState } from 'react';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { GET_DEVICES_LIST, GET_HEARTBEAT_STATUS } from '../query/query';
 
 const columns: GridColDef[] = [
   { field: 'deviceid', headerName: 'Device ID', width: 120 },
@@ -10,31 +11,56 @@ const columns: GridColDef[] = [
   { field: 'devicename', headerName: 'Device Name', width: 180 },
   { field: 'devicelocation', headerName: 'Device Location', width: 180 },
   { field: 'deviceip', headerName: 'Device IP', width: 150 },
+  
 ];
 
 const Home = () => {
-  const orgCode = "d3b6842d"; 
+  const orgCode = "d3b6842d";
+  const [deviceCode, setDeviceCode] = useState("DM_HY_D01");
 
-  const { loading, error, data } = useQuery(GET_DEVICES_LIST, {
-    variables: { input: { orgcode: orgCode } },
+  const { loading: devicesLoading, error: devicesError, data: devicesData} = useQuery(GET_DEVICES_LIST, {
+    variables: { input: { orgcode: orgCode, devicecode: deviceCode } },
   });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  const [getHeartbeatStatus] = useMutation(GET_HEARTBEAT_STATUS, {
+    onError: (error) => {
+      console.error("Error fetching heartbeat status:", error);
+    },
+    onCompleted: (data) => {
 
-  const rows = data?.getLcdeviceList.map((device:any, index:any) => ({
-    ...device,
-    id: index + 1, 
-  })) || [];
+      if(data.getHeartbeat.data){
+        const parsedData = JSON.parse(data.getHeartbeat.data);
+          console.log("heartbeat Status : ", parsedData); 
+      }
+    
+    },
+  });
+
+  const handleHeartbeatClick = () => {
+    getHeartbeatStatus({
+      variables: {
+        input: {
+          orgcode: orgCode,
+          devicecode: deviceCode,
+        },
+      },
+    });
+  };
+
+  if (devicesLoading) return <p>Loading devices...</p>;
+  if (devicesError) return <p>Error loading devices: {devicesError.message}</p>;
 
   return (
     <div style={{ marginTop: '4rem' }}>
       <h3>List of LC Devices</h3>
+      <button onClick={handleHeartbeatClick} style={{ marginTop: '1rem', marginBottom: '2rem', backgroundColor: 'green', color: 'white' }}>Heartbeat Status</button>
       <div style={{ height: "80vh", width: '100%' }}>
-        <DataGrid
-          rows={rows}
+        <DataGrid 
+          rows={devicesData?.getLcdeviceList.map((device: any, index: number) => ({
+            ...device,
+            id: index + 1,
+          })) || []}
           columns={columns}
-         
         />
       </div>
     </div>
